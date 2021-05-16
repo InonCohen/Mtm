7ce710edfd408f98a27f247edbeaa16fc4c905f6
InonCohen <73906379+InonCohen@users.noreply.github.com>
2021-05-16 12:02:11 +0300

    trying to stash things

---
+++ ex1/map/LinkedList.h
@@ -0,0 +1,23 @@
+#ifndef EX1NEW_LINKEDLIST_H
+#define EX1NEW_LINKEDLIST_H
+/**
+* Generic List Container
+*
+* Implements a list container type.
+*
+* The following functions are available:
+*   listCreate		- Creates a new List
+*   listDestroy		- Deletes an existing list and frees all resources
+*   listCopy		- Copies an existing list
+*   listGetsize     - Returns the size of the list
+*   listGetNode     - Returns a pointer to an existing node in the list
+*   listInsert      - Puts a new node at the end of the list, or changes data
+*                     of an existing node
+*   listContains    - Returns TRUE if a specific key exists in
+*   listSetData     - Changes the data of an existing list
+*   listGetNext     - Returns the pointer of the next list in the list
+*   listSetNext     - Changes the pointer of the next list in the list
+*   listGetPrev     - Returns the pointer of the previous list in the list
+*   listSetPrev     - Changes the pointer of the previous list in the list
+*/
+#endif //EX1NEW_LINKEDLIST_H
