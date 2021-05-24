ab303549dac9474842200701fd169cdb83785d52
Ohad Benacot <ohad.benacot@campus.technion.ac.il>
2021-05-24 15:15:22 +0300

    git ignore file-main

--- ex1/chessGame.c
+++ ex1/chessGame.c
@@ -1,15 +1,37 @@
 #include "chessGame.h"
 #include "chessSystem.h"
+#include "assert.h"
+#include "stdlib.h"
 
 struct ChessGame{
-    unsigned int* gameID;
-    unsigned int* playersIDS;
+    unsigned int gameID;
+    unsigned int* playersIDs;
     int playTime;
     Winner gameWinner;
 };
 
-ChessGame chessGameCreate();
+ChessGame chessGameCreate(unsigned int gameID, unsigned int* playersIDs, int playTime, Winner gameWinner){
+    ChessGame result = malloc(sizeof (*result));
+    if (!result){
+        return NULL;
+    }
+    re
+    ChessGame result = {gameID, playersIDs, playTime, gameWinner};
+    return result;
+}
 
-ChessGame chessGameCopy(ChessGame);
+ChessGame chessGameCopy(ChessGame game){
+    assert (game != NULL);
+    ChessGame newGame;
+    ChessGame newGame = chessGameCreate(game->gameID, game->playersIDs, game->playTime, game->gameWinner);
+    if (newGame == NULL) {
+        return NULL;
+    }
+    if (addAllOrDestroy(newChessGame, chessGame) == CHESS_OUT_OF_MEMORY) {
+        return NULL;
+    }
+    newGame->iterator = chessGame->iterator;
+    return newGame;
+}
 
 void chessGameDestroy(ChessGame);
