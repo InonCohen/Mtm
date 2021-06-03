34086fc9dc154c1eb1dd5d2e3ef186ca23090e03
ohadbenacot <ohad.benacot@gmail.com>
2021-06-04 00:40:21 +0300

    .

---
+++ ex1/chess/cmake-build-debug-csl3/CMakeFiles/chessSystem.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/DLLNode.c" "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/cmake-build-debug-csl3/CMakeFiles/chessSystem.dir/DLLNode.c.o"
+  "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/LinkedList.c" "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/cmake-build-debug-csl3/CMakeFiles/chessSystem.dir/LinkedList.c.o"
+  "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/chessGame.c" "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/cmake-build-debug-csl3/CMakeFiles/chessSystem.dir/chessGame.c.o"
+  "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/chessMapUtils.c" "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/cmake-build-debug-csl3/CMakeFiles/chessSystem.dir/chessMapUtils.c.o"
+  "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/chessPlayer.c" "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/cmake-build-debug-csl3/CMakeFiles/chessSystem.dir/chessPlayer.c.o"
+  "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/chessPlayerID.c" "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/cmake-build-debug-csl3/CMakeFiles/chessSystem.dir/chessPlayerID.c.o"
+  "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/chessSystem.c" "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/cmake-build-debug-csl3/CMakeFiles/chessSystem.dir/chessSystem.c.o"
+  "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/chessTournament.c" "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/cmake-build-debug-csl3/CMakeFiles/chessSystem.dir/chessTournament.c.o"
+  "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/map.c" "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/cmake-build-debug-csl3/CMakeFiles/chessSystem.dir/map.c.o"
+  "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/strUtils.c" "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/cmake-build-debug-csl3/CMakeFiles/chessSystem.dir/strUtils.c.o"
+  "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/test/chessSystemTestsExample.c" "/tmp/tmp.YVaeKa3iS7/tmp/tmp.YVaeKa3iS7/chess/cmake-build-debug-csl3/CMakeFiles/chessSystem.dir/test/chessSystemTestsExample.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../map"
+  "../chess"
+  "../chess/test"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
