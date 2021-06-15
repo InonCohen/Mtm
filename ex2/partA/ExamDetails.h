6ab7e3793b66af10d69cc46fa229b295b2b3dabd
ohadbenacot <ohad.benacot@gmail.com>
2021-06-15 23:51:35 +0300

    Review ExamDetails

--- ex2/partA/ExamDetails.h
+++ ex2/partA/ExamDetails.h
@@ -1,38 +1,51 @@
 #ifndef EX1_EXAMDETAILS_H
 #define EX1_EXAMDETAILS_H
-#include <stdio>
-#include <stdlib>
+#include <cstdio>
+#include <string>
+#include <cstdlib>
 
 
 namespace mtm{
-    class ExamTime{
-        int month;
-        int day;
-        double time;
-    public:
-        ExamTime(int month, int day, double time);
-        ~ExamTime()=default;
-        ExamTime& operator=(const ExamTime& other);
-    };
 
         class ExamDetails{
             int course_number;
-            ExamTime exam_time;
+            int month;
+            int day;
+            double time;
             int duration;
             std::string zoom_link;
         public:
-            ExamDetails(int course_number, int month, int day, double time, int duration, std::string zoom_link);
+            class InvalidDateException: public std::exception{
+            public:
+                const char* what() const noexcept {
+                    return "invalid date";
+                }
+            };
+            class InvalidTimeException: public std::exception{
+            public:
+                const char* what() const noexcept {
+                    return "invalid time";
+                }
+            };
+            class InvalidArgsException: public std::exception{
+            public:
+                const char* what() const noexcept {
+                    return "invalid argument";
+                }
+            };
+            ExamDetails(int course_number, int month, int day, double time, int duration, std::string zoom_link="");
             ~ExamDetails();
-            ExamDetails(const ExamDetails& exam);
-            ExamDetails& operator=(const ExamDetails& exam);
-            std::string& getLink() const;
-            void setLink(const std::string& new_link);
-            int operator-(const ExamDetails& exam) const;
-            bool operator<(const ExamDetails& exam) const;
-            friend std::ostream& operator<<(std::ostream&, const ExamDetails& exam);
-            ExamDetails makeMatamExam();
+            ExamDetails(const ExamDetails& other);
+            ExamDetails& operator=(const ExamDetails& other);
+            std::string getLink() const;
+            void setLink(const std::string new_link);
+            int operator-(const ExamDetails& other) const;
+            bool operator<(const ExamDetails& other) const;
+            static ExamDetails makeMatamExam();
+            friend std::ostream& operator<<(std::ostream& os, const ExamDetails& exam);
+
         };
-        std::ostream& ExamDetails::operator<<(std::ostream& os, const ExamDetails& exam);
+        std::ostream& operator<<(std::ostream& os, const ExamDetails& exam);
 
 
 }
