5568d7424f047c3b9f6d7edfe420883bc8820e4c
ohadbenacot <ohad.benacot@gmail.com>
2021-06-29 18:43:19 +0300

    Finished Ex3 Tests

--- ex3/gradesCalc.py
+++ ex3/gradesCalc.py
@@ -3,45 +3,51 @@
 # rows from the input file) into the file in `output_path`. Returns the average of the grades.
 #   input_path: Path to the file that contains the input
 #   output_path: Path to the file that will contain the output
+
+
 def final_grade(input_path: str, output_path: str) -> int:
     separator = ','
     sum_valid_grades = 0
-    num_valid_students = 0
     student_data_keys = ['name', 'semester', 'homework_avg']
+    output_data = dict()
+    with open(input_path) as input_file:
+        lines = input_file.readlines()
+    for line in lines:
+        input_data_values = list(filter(lambda char: char != separator, line.split(separator)))
+        input_data_values = list(map(str.strip, input_data_values))
+        student_id = input_data_values[0].strip()
+        student_data_values = input_data_values[1:]
+        student_data = dict(zip(student_data_keys, student_data_values))
+        if is_valid_input(student_id, student_data):
+            student_final_grade = calculate_final_grade(student_id, student_data)
+            student_str = "{student_id}, {homework_avg}, {student_final_grade}\n". \
+                format(student_id=student_id,
+                       homework_avg=student_data["homework_avg"],
+                       student_final_grade=student_final_grade)
+            output_data[student_id] = student_str
+            sum_valid_grades += student_final_grade
 
-    with open(output_path, 'a') as output_file:
-        with open(input_path) as input_file:
-            line_str = input_file.readline()
-            input_data_values = list(filter(lambda char: char != separator, line_str.split(separator)))
-            student_id = int(input_data_values[0])
-            student_data_values = input_data_values[1:]
-            student_data = dict(zip(student_data_keys, student_data_values))
-            if is_valid_student_data(student_data):
-                student_final_grade = calculate_final_grade(student_id, student_data)
-                student_data_str = "{student_id}, {homework_avg}, {student_final_grade}\n". \
-                    format(student_id=student_id,
-                           homework_avg=student_data["homework_avg"],
-                           student_final_grade=student_final_grade)
-                output_file.write(student_data_str)
-                sum_valid_grades += student_data["homework_avg"]
-                num_valid_students += 1
-
-    if num_valid_students > 0:
-        return int(sum_valid_grades / num_valid_students)
+    with open(output_path, 'w') as output_file:
+        output_file.writelines(sorted(output_data.values()))
+    if len(output_data) > 0:
+        return int(sum_valid_grades / len(output_data))
     return sum_valid_grades
 
 
-def is_valid_student_data(student_data: dict) -> bool:
+def is_valid_input(student_id: str, student_data: dict) -> bool:
+    min_valid_grade = 50
+    max_valid_grade = 100
     return all([
-        int(student_data.get('id')[0]) != 0,
+        len(str(student_id)) == 8,
+        int(str(student_id)[0]) != 0,
         student_data.get('name').replace(' ', '').isalpha(),
         int(student_data.get('semester')) >= 1,
-        50 < int(student_data.get('homework_avg')) <= 100
+        min_valid_grade < int(student_data.get('homework_avg')) <= max_valid_grade
     ])
 
 
-def calculate_final_grade(student_id: int, student_data: dict) -> int:
-    return int(int(str(student_id)[-2:]) + int(student_data['homework_avg']) / 2)
+def calculate_final_grade(student_id: str, student_data: dict) -> int:
+    return int((int(student_id[-2:]) + int(student_data['homework_avg'])) / 2)
 
 
 #### PART 2 ####
@@ -49,5 +55,10 @@ def calculate_final_grade(student_id: int, student_data: dict) -> int:
 #   s1: The string that we want to check if it can be constructed
 #   s2: The string that we want to construct s1 from
 def check_strings(s1: str, s2: str) -> bool:
-    # TODO: implement here
-    raise NotImplementedError
+    letters = list(char for char in s2.lower())
+    for letter in s1.lower():
+        if letter in letters:
+            letters.remove(letter)
+        else:
+            return False
+    return True
--- ex3/test.py
+++ ex3/test.py
@@ -1,6 +1,50 @@
 from gradesCalc import *
 
 
+def grades_calc_hemi_test():
+    # imports for hemi tests
+    import os
+    import difflib
+
+    # constants
+    hemi_path = "/Users/ohadbenacot/CLionProjects/Mtm/ex3/tests/Agressive_tests_Hemi_grades"
+    input_basename = "input"
+    output_basename = "output"
+    my_files_basename = "MY"
+    filename_extension = ".txt"
+    average_filename = "Averages.txt"
+    diff_filename = "diff.txt"
+    average_test_str = "Average test{test_num}: {course_avg}\n"
+    diff_prompt = "\nDiff Of Input{}\n"
+
+    # Executing Hemi tests into diff files
+    os.chdir(hemi_path)
+    files = os.listdir()
+    inputs = list(filter(lambda x: x.startswith(input_basename), files))
+    outputs = list(filter(lambda x: x.startswith(output_basename), files))
+
+    with open(average_filename, 'a') as avg_file:
+        avg_file.write("\n\nResults\n------\n")
+        for input_file in sorted(inputs):
+            input_num = input_file.strip(input_basename).strip(filename_extension)
+            my_output_file = "{}{}{}{}".format(my_files_basename, output_basename, input_num, filename_extension)
+            course_avg = final_grade(input_path=input_file, output_path=my_output_file)
+            avg_file.write(average_test_str.format(test_num=input_num, course_avg=course_avg))
+
+    for output_file in sorted(outputs):
+        output_num = output_file.strip(output_basename).strip(filename_extension)
+        my_output_file = "{}{}{}{}".format(my_files_basename, output_basename, output_num, filename_extension)
+        with open(output_file, 'r') as src_output_file_reader:
+            lines = src_output_file_reader.readlines()
+        with open(my_output_file, 'r') as source_output_file_reader:
+            src_lines = source_output_file_reader.readlines()
+
+        with open(diff_filename, 'a') as diff_file_writer:
+            diff_file_writer.write(diff_prompt.format(output_num))
+            for line in difflib.unified_diff(lines, src_lines, lineterm=''):
+                diff_file_writer.write(line)
+
+
 # Testing your implemented functions, feel free to add more tests below
 def main():
     # Testing the `final_grade` function
@@ -14,6 +58,8 @@ def main():
     s2 = 'baNaNa'
     result = check_strings(s1=s1, s2=s2)
     assert result
+
+    grades_calc_hemi_test()
 
 
 if __name__ == "__main__":
